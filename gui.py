7753fe660b7657c1a9c355cf05938a54c1fc91e6
delow <youhouma@student.1337.ma>
2020-07-09 07:36:31 -0700

    second commit

---
+++ gui.py
@@ -0,0 +1,53 @@
+from flask import Flask, Response, request, url_for, redirect, render_template
+from netmiko import ConnectHandler
+import sys
+import os
+import ast
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "hello"
+@app.route('/vlan', methods=['GET', 'POST'])
+def vlan_creation():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(f.filename)
+        file = open(f.filename, "r")
+        contents = file.read()
+        dict = ast.literal_eval(contents)
+        x = 0
+        exp = []
+        check = 0
+        lst = []
+        i = 0
+        limit = len(dict)
+        while( limit != 0):
+            x += 1
+            print('""""""""""""""""configuration for switch make => ' + str(x) + '\n')
+            print(dict[limit])
+            index_x = int(request.form['indexX'])
+            index_y= int(request.form['indexY'])
+            lst = ast.literal_eval(request.form['novlan'])
+            n = len(lst)
+            net_connect = ConnectHandler(**dict[limit])
+            vlan_names = request.form['vlanN']
+            for n in range (index_x, index_y):
+              while(len(lst) > i):
+                if(lst[i] == n):
+                    check = 1
+                    break
+                else:
+                    check = 0
+                i += 1
+              i = 0
+              if(check == 0):
+                print ('creating vlan ' + str(n))
+                config_commands = ['vlan ' + str(n), 'name ' + vlan_names +str(n)]
+                output = net_connect.send_config_set(config_commands)
+                print (output)
+            limit -= 1
+        return 'DONE' 
+
+    return (render_template('template.html'))
+if __name__ == '__main__':
+      app.run(host='0.0.0.0', port=80, debug=True)
