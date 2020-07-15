b246cb6a40ade978fcaf1aaa26a92042727102d6
dolew <youhouma@student.1337.ma>
2020-07-15 15:14:10 +0000

    update

--- gui.py
+++ gui.py
@@ -22,12 +22,15 @@ def vlan_creation():
         i = 0
         limit = len(dict)
         while( limit != 0):
+            index1 = 0
             x += 1
             print('""""""""""""""""configuration for switch make => ' + str(x) + '\n')
             print(dict[limit])
             index_x = int(request.form['indexX'])
             index_y= int(request.form['indexY'])
             lst = ast.literal_eval(request.form['novlan'])
+            lst1 = ast.literal_eval(request.form['interface'])
+            index2 = len(lst1[index1]) - 1
             n = len(lst)
             net_connect = ConnectHandler(**dict[limit])
             vlan_names = request.form['vlanN']
@@ -45,9 +48,17 @@ def vlan_creation():
                 config_commands = ['vlan ' + str(n), 'name ' + vlan_names +str(n)]
                 output = net_connect.send_config_set(config_commands)
                 print (output)
+            while(index2 != -1):
+                print('...........trunking setup for ' + lst1[index1][index2] + '..............')
+                config_command = ['int ' + lst1[index1][index2], 'switchport trunk encapsulation dot1q']
+                output = net_connect.send_config_set(config_command)
+                index2 -= 1
+                print (output)
             limit -= 1
+            index1 += 1
         return 'DONE' 
 
     return (render_template('template.html'))
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=80, debug=True)
+
